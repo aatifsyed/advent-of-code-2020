4b5cf443513ee4d6360971265170147c69e96c17
Aatif Syed <aatif.syed@metaswitch.com>
2020-12-01 15:43:56 +0000

    Read numbers from file

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,21 +1,6 @@
 use fileutils;
-use std::fs::File;
-use std::io;
-use std::io::prelude::*;
-
-fn read_username_from_file() -> Result<String, io::Error> {
-    let mut f = File::open("username.txt")?;
-    let mut s = String::new();
-
-    f.read_to_string(&mut s)?;
-
-    Ok(s)
-}
 
 fn main() {
-    fileutils::foo();
-    match read_username_from_file() {
-        Ok(_) => println!("Success!"),
-        Err(_) => println!("Failed!"),
-    }
+    let nums = fileutils::numbers_from_file("inputs/day01.txt");
+    println!("{:?}", nums);
 }
--- fileutils/src/lib.rs
+++ fileutils/src/lib.rs
@@ -5,7 +5,23 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+use std::fs;
+use std::io;
+use std::io::BufRead;
+use std::path;
 
-pub fn foo() {
-    println!("foo!")
+/// https://stackoverflow.com/a/35820003
+pub fn lines_from_file(filename: impl AsRef<path::Path>) -> Vec<String> {
+    let file = fs::File::open(filename).expect("no such file");
+    let buf = io::BufReader::new(file);
+    buf.lines()
+        .map(|line| line.expect("Could not parse line"))
+        .collect()
+}
+
+pub fn numbers_from_file(filename: impl AsRef<path::Path>) -> Vec<isize> {
+    lines_from_file(filename)
+        .into_iter()
+        .map(|line| line.parse::<isize>().expect("Couldn't parse line"))
+        .collect()
 }
