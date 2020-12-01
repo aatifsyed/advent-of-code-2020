7456e6a8ca99a50d1e39c9aa050d6ad801989fd3
Aatif Syed <aatif.syed@metaswitch.com>
2020-12-01 16:07:05 +0000

    day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -15,8 +15,25 @@ fn part1(filepath: &str) -> isize {
     panic!("Couldn't find a match!")
 }
 
+fn part2(filepath: &str) -> isize {
+    let nums = fileutils::numbers_from_file(filepath);
+
+    // https://doc.rust-lang.org/rust-by-example/flow_control/loop/nested.html
+    for a in &nums {
+        for b in &nums {
+            for c in &nums {
+                if a + b + c == 2020 {
+                    return a * b * c;
+                }
+            }
+        }
+    }
+    panic!("Couldn't find a match!")
+}
+
 fn main() {
-    println!("{}", part1("inputs/day01.txt"));
+    println!("part 1: {}", part1("inputs/day01.txt"));
+    println!("part 2: {}", part2("inputs/day01.txt"));
 }
 
 #[cfg(test)]
@@ -25,5 +42,9 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1("../inputs/day01.txt"), 158916);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("../inputs/day01.txt"), 165795564);
     }
 }
