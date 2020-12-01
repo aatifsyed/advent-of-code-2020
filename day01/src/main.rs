41d8d64daf60aaaa305f3c3ef0b67e24711f121f
Aatif Syed <aatif.syed@metaswitch.com>
2020-12-01 16:04:50 +0000

    day 1 part 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,6 +1,29 @@
 use fileutils;
 
+// Accept filename due to https://github.com/rust-lang/cargo/issues/8340
+fn part1(filepath: &str) -> isize {
+    let nums = fileutils::numbers_from_file(filepath);
+
+    // https://doc.rust-lang.org/rust-by-example/flow_control/loop/nested.html
+    for a in &nums {
+        for b in &nums {
+            if a + b == 2020 {
+                return a * b;
+            }
+        }
+    }
+    panic!("Couldn't find a match!")
+}
+
 fn main() {
-    let nums = fileutils::numbers_from_file("inputs/day01.txt");
-    println!("{:?}", nums);
+    println!("{}", part1("inputs/day01.txt"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("../inputs/day01.txt"), 158916);
+    }
 }
