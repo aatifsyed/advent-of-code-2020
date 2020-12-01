ebe79a6fdf7ab8bdb1507368ab39d9f82014073f
Aatif Syed <aatif.syed@metaswitch.com>
2020-12-01 14:42:54 +0000

    Got the hang of ?

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,8 +1,19 @@
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("username.txt")?;
+    let mut s = String::new();
+
+    f.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
 fn main() {
-    println!("Hello, day01!");
-    let mut file = File::create("day1.file").expect("File error");
-    file.write_all(b"Hello, day01!").expect("Write error");
+    match read_username_from_file() {
+        Ok(_) => println!("Success!"),
+        Err(_) => println!("Failed!"),
+    }
 }
