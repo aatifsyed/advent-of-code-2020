7c44a9ddf15b3866b706e96ebc52324c8490a9a2
Aatif Syed <aatif.syed@metaswitch.com>
2020-12-03 18:30:37 +0000

    Can parse a password entry

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,16 +1,49 @@
 #[macro_use]
 extern crate lazy_static;
-use regex::{Captures, Match, Regex};
+use regex::Regex;
 
+#[derive(Debug, Eq, PartialEq)]
 struct PasswordRule {
     min_occurences: usize,
     max_occurences: usize,
     letter: char,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 struct PasswordEntry {
     rule: PasswordRule,
     password: String,
+}
+
+impl PasswordEntry {
+    fn from_line(line: &str) -> PasswordEntry {
+        let captures = PASSWORD_ENTRY_REGEX
+            .captures(line)
+            .expect("Couldn't regex on line");
+        PasswordEntry {
+            rule: PasswordRule {
+                min_occurences: captures
+                    .name("min")
+                    .expect("No min!")
+                    .as_str()
+                    .parse::<usize>()
+                    .expect("Couldn't parse min"),
+                max_occurences: captures
+                    .name("max")
+                    .expect("No max!")
+                    .as_str()
+                    .parse::<usize>()
+                    .expect("Couldn't parse max"),
+                letter: captures
+                    .name("letter")
+                    .expect("No letter!")
+                    .as_str()
+                    .parse::<char>()
+                    .expect("Couldn't parse letter"),
+            },
+            password: String::from(captures.name("password").expect("No password!").as_str()),
+        }
+    }
 }
 
 lazy_static! {
@@ -19,13 +52,23 @@ lazy_static! {
             .expect("Couldn't compile regex");
 }
 
-fn main() {
-    // PASSWORD_ENTRY_REGEX.name("fuck")
-}
+fn main() {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn captures_line() {}
+    fn captures_line() {
+        assert_eq!(
+            PasswordEntry::from_line("7-8 p: ppppppdx"),
+            PasswordEntry {
+                rule: PasswordRule {
+                    min_occurences: 7,
+                    max_occurences: 8,
+                    letter: 'p'
+                },
+                password: String::from("ppppppdx")
+            }
+        )
+    }
 }
