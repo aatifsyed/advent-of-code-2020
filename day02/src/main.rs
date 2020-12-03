99ed0ca2998f4c64654421bba3cbe75daf4dd1c8
Aatif Syed <aatif.syed@metaswitch.com>
2020-12-03 20:12:04 +0000

    day 2 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -5,8 +5,14 @@ use regex::Regex;
 
 #[derive(Debug, Eq, PartialEq)]
 struct PasswordRule {
-    min_occurences: usize,
-    max_occurences: usize,
+    /// For the rental, this is the minumum occurences.
+    ///
+    /// For OTC, this is the first position
+    a: usize,
+    /// For the rental, this is the maximum occurences.
+    ///
+    /// For OTC, this is the second position
+    b: usize,
     letter: char,
 }
 
@@ -42,16 +48,21 @@ impl PasswordEntry {
             .expect(&format!("Couldn't parse line {}!", line));
         PasswordEntry {
             rule: PasswordRule {
-                min_occurences: captures.extract_captured("min"),
-                max_occurences: captures.extract_captured("max"),
+                a: captures.extract_captured("min"),
+                b: captures.extract_captured("max"),
                 letter: captures.extract_captured("letter"),
             },
             password: captures.extract_captured("password"),
         }
     }
-    fn is_valid(&self) -> bool {
+    fn is_valid_at_rental(&self) -> bool {
         let num_matches = self.password.matches(self.rule.letter).count();
-        num_matches >= self.rule.min_occurences && num_matches <= self.rule.max_occurences
+        num_matches >= self.rule.a && num_matches <= self.rule.b
+    }
+    fn is_valid_at_otc(&self) -> bool {
+        let chars: Vec<char> = self.password.chars().collect();
+        // Convert from 1-based index (OTC rule) to 0-based
+        (chars[self.rule.a - 1] == self.rule.letter) ^ (chars[self.rule.b - 1] == self.rule.letter)
     }
 }
 
@@ -64,13 +75,20 @@ lazy_static! {
 fn part1(filepath: &str) -> usize {
     fileutils::lines_from_file(filepath)
         .into_iter()
-        .filter(|s| PasswordEntry::from_line(s).is_valid())
+        .filter(|s| PasswordEntry::from_line(s).is_valid_at_rental())
+        .count()
+}
+
+fn part2(filepath: &str) -> usize {
+    fileutils::lines_from_file(filepath)
+        .into_iter()
+        .filter(|s| PasswordEntry::from_line(s).is_valid_at_otc())
         .count()
 }
 
 fn main() {
     println!("part1: {}", part1("inputs/day02.txt"));
-    // println!("part2: {}", part2("inputs/day02.txt"));
+    println!("part2: {}", part2("inputs/day02.txt"));
 }
 
 #[cfg(test)]
@@ -82,25 +100,36 @@ mod tests {
             PasswordEntry::from_line("7-8 p: ppppppdx"),
             PasswordEntry {
                 rule: PasswordRule {
-                    min_occurences: 7,
-                    max_occurences: 8,
+                    a: 7,
+                    b: 8,
                     letter: 'p'
                 },
                 password: String::from("ppppppdx")
             }
         )
     }
-
     #[test]
-    fn recognises_valid() {
-        assert!(PasswordEntry::from_line("1-1 a: abc").is_valid())
+    fn recognises_valid_rental() {
+        assert!(PasswordEntry::from_line("1-1 a: abc").is_valid_at_rental())
     }
     #[test]
-    fn recognises_invalid() {
-        assert!(!PasswordEntry::from_line("0-0 a: abc").is_valid())
+    fn recognises_invalid_rental() {
+        assert!(!PasswordEntry::from_line("0-0 a: abc").is_valid_at_rental())
     }
     #[test]
     fn test_part1() {
         assert_eq!(part1("../inputs/day02.txt"), 536)
+    }
+    #[test]
+    fn recognises_valid_otc() {
+        assert!(PasswordEntry::from_line("1-3 a: abc").is_valid_at_otc())
+    }
+    #[test]
+    fn recognises_invalid_otc() {
+        assert!(!PasswordEntry::from_line("1-3 a: aba").is_valid_at_otc())
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("../inputs/day02.txt"), 558)
     }
 }
